af3075bf320450702eaa9fa970167c603a32bad7
yuu-2020 <s206006@wakayama-u.ac.jp>
2020-11-08 20:57:37 +0900

    ARViewをモーダルで遷移するように変更

--- eZukaAR/Controller/ARViewController.swift
+++ eZukaAR/Controller/ARViewController.swift
@@ -12,6 +12,7 @@ import RealityKit
 class ARViewController: UIViewController {
 
     @IBOutlet weak var arView: ARView!
+    @IBOutlet weak var homeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,16 @@ class ARViewController: UIViewController {
         guard let anchor = try? EzukaMain.loadMain() else { return }
         
         arView.scene.addAnchor(anchor)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    @IBAction func homeButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
 
---
+++ eZukaAR/Controller/MainTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  MainTabBarController.swift
+//  eZukaAR
+//
+//  Created by AsanoYudai on 2020/11/07.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        delegate = self
+    }
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    // TabBarItemが選択された時に呼ばれる
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        // TabBarItemタップでModal表示をする画面を指定して実装
+        if viewController is ARViewController {
+            let storyboard: UIStoryboard = self.storyboard!
+            let second = storyboard.instantiateViewController(withIdentifier: "ar")
+            self.present(second, animated: true, completion: nil)
+            return false
+        }
+        return true
+    }
+}
