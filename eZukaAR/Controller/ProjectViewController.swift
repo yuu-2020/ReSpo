41be82cb7e31bc901b7b169b77bc69b664d39ba7
NanakoShimizu <radius775@gmail.com>
2020-11-13 20:40:40 +0900

    デバイスのDocumentディレクトリ内のファイルに合わせてプロジェクトに一覧で表示されるように変更 ・controller viewで意図しない順番で表示されるバグ ・アプリを立ち上げ直さないとプロジェクトの中身が更新されないバグ を内包

--- eZukaAR/Controller/ProjectViewController.swift
+++ eZukaAR/Controller/ProjectViewController.swift
@@ -9,21 +9,22 @@
 import UIKit
  
 class ProjectViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
-    
-    // 画像
-    // let images = ["BasketCard", "BasketCard_edit"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 180, height: 260)
+        return CGSize(width: 100, height: 150)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Documentディレクトリにあるファイルの名前一覧を配列で取得
+        let fileNames = getFileNmaes()
+        // 配列に含まれる数を保持
+        let numberOfFiles = fileNames.count
         // return images.count // 表示するセルの数
-        return 1
+        return numberOfFiles
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -31,9 +32,10 @@ class ProjectViewController: UIViewController, UICollectionViewDataSource, UICol
         
         // Tag番号を使ってImageViewのインスタンス生成
         let imageView = cell.contentView.viewWithTag(1) as! UIImageView
-        // 画像配列の番号で指定された要素の名前の画像をUIImageとする
-        // let cellImage = UIImage(named: images[indexPath.row])
-        let cellImage = readimage()
+
+        let fileNames = getFileNmaes()
+        // 画像を読み込む
+        let cellImage = readImage(fileName: fileNames[indexPath.row])
         // UIImageをUIImageViewのimageとして設定
         imageView.image = cellImage
         
@@ -44,10 +46,26 @@ class ProjectViewController: UIViewController, UICollectionViewDataSource, UICol
         return 1
     }
     
-    func readimage() -> UIImage?  {
+    func readImage(fileName: String) -> UIImage?  {
         let documentsURL:URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let fileURL:URL = documentsURL.appendingPathComponent("test.png")
+        // let cardName = "Test1.png"
+        let fileURL:URL = documentsURL.appendingPathComponent(fileName)
         return UIImage(contentsOfFile: fileURL.path)
+    }
+    
+    // Documentディレクトリ内のファイル名を取得
+    func getFileNmaes() -> [String] {
+        let documentDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        do {
+            let contentUrls = try FileManager.default.contentsOfDirectory(at: documentDirectoryURL, includingPropertiesForKeys: nil)
+            let files = contentUrls.map{$0.lastPathComponent}
+            // print(files)
+            return files
+        } catch {
+            print(error)
+            return []
+        }
+    
     }
     
 }
